9de43c64ad5e25386ba106b5599412825b60170f
Franco Carrara <frankko212@gmail.com>
2022-09-21 20:12:09 -0300

    Change semantics

--- src/components/Patient.jsx
+++ src/components/Patient.jsx
@@ -1,12 +1,12 @@
 function Patient(props){
     return(
-    <div className="bg-white w-full mt-4 rounded-md shadow-md p-6">
+    <article className="bg-white w-full mt-4 rounded-md shadow-md p-6">
         <p className="uppercase font-bold text-gray-700">Pet's name: <span className="normal-case font-normal">{props.name}</span></p>
         <p className="uppercase font-bold text-gray-700">Owner's name: <span className="normal-case font-normal">{props.owner}</span></p>
         <p className="uppercase font-bold text-gray-700">Email: <span className="normal-case font-normal">{props.email}</span></p>
         <p className="uppercase font-bold text-gray-700">Date: <span className="normal-case font-normal">{props.date}</span></p>
         <p className="uppercase font-bold text-gray-700">Current symptoms: <span className="normal-case font-normal">{props.symptoms}</span></p>
-    </div>
+    </article>
     )
 }
 
