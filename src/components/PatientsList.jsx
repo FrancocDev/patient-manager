73ccfcc9853440a90e2452a80d275731b9b483fe
Franco Carrara <frankko212@gmail.com>
2022-09-23 22:55:56 -0300

    Conditional text when there are no patients

--- src/components/PatientsList.jsx
+++ src/components/PatientsList.jsx
@@ -14,7 +14,7 @@ function PatientList(props){
 
     return(
         <div className="md:w-1/2 lg:w-3/5 p-6 flex flex-col items-center">
-            <h2 className="font-black text-3xl text-center">Patient's list</h2>
+            <h2 className="font-black text-3xl text-center">{props.patients.length > 0 ? "Patient's list" : "There are no patients"}</h2>
             <p className="mt-2">Manage your <span className="text-indigo-500 font-bold">patients and appointments</span></p>
             {props.patients.length > 0 && RenderPatients}
         </div>
