716134e43699093e721301bb0f713df15c34094a
Franco Carrara <frankko212@gmail.com>
2022-09-24 19:46:00 -0300

    Add margin top to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header(){
     return(
-        <h1 className="font-black text-5xl text-center mx-auto">
+        <h1 className="font-black text-5xl text-center mx-auto mt-4">
             Patients follow-up <br />
             <span className="text-indigo-600">Veterinary</span>
         </h1>
